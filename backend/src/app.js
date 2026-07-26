1f3f93dfbadcacb7d7b9f436744b06cd887d367e
rana-ji0001 <karanrana705021@gmail.com>
2026-07-26 22:32:53 +0530

    cors api for vercel

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,8 +7,11 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
+    origin: [
+        "http://localhost:5173",
+        "https://cartivo-sepia.vercel.app/"
+    ],
+    credentials: true
 }));
 
 
