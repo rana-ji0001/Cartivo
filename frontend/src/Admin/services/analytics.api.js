63048561d37887aff486fa434226683af3383e9e
rana-ji0001 <karanrana705021@gmail.com>
2026-07-26 22:04:40 +0530

    backend render api

--- frontend/src/Admin/services/analytics.api.js
+++ frontend/src/Admin/services/analytics.api.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
+const API_URL = import.meta.env.VITE_API_URL;
 
 
 const api = axios.create({
-    baseURL:"http://localhost:3000/api/analytics",
+    baseURL:`${API_URL}/api/analytics`,
     withCredentials:true
 });
 
