bdcfc72fad6f42afd5bf3eb8587206ca62119c34
rana-ji0001 <karanrana705021@gmail.com>
2026-07-26 22:36:51 +0530

    same

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,8 +8,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
     origin: [
-        "http://localhost:5173",
-        "https://cartivo-sepia.vercel.app/"
+        "https://cartivo-sepia.vercel.app/",
+        "http://localhost:5173"
     ],
     credentials: true
 }));
